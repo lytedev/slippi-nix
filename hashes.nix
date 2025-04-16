1e745c0887b798e3b0c15a10407039a35a29e091
~noodlez1232 <contact@nathanielbarragan.xyz>
2025-04-15 18:09:17 -0700

    refactor project
    Refactor the project
    - Move each package to their own file
    - Change the packages' downloading method to expose the zip file it
      downloads (for use by the home manager module)
    - Move all the hashes for the packages to its own file.
    - Move the modules to their own files
    - Clean up the flake.nix itself
    - BONUS: update slippi-launcher to v2.11.9

---
+++ hashes.nix
@@ -0,0 +1,18 @@
+{
+  netplay = {
+    version = "3.4.5";
+    hash = "sha256-XspvaRlLNAeJ2KyagS4PWOqaJHVZqvw/a3Z3mAxOFJI=";
+  };
+  netplay-beta = {
+    version = "4.0.0-mainline-beta.9";
+    hash = "sha256-gG7/E4fkAo7P/i2or5J8sQrriqSQducOytN+oP0+BfA=";
+  };
+  playback = {
+    version = "3.4.5";
+    hash = "sha256-iCBdlcBPSRT8m772sqI+gSfNmVNAug0SfkSwVUE6+fE=";
+  };
+  launcher = {
+    version = "2.11.9";
+    hash = "sha256-ocKM8m1OCJTaWUJC9Gat5V2mIyzamNxkFt+3LW6FZ3k=";
+  };
+}
