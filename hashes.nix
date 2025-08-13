9e31416b34326b9f3d1de16b97935b49568835b5
Daniel Flanagan <daniel@lyte.dev>
2025-08-13 13:42:40 -0500

    chore: update

--- hashes.nix
+++ hashes.nix
@@ -1,15 +1,15 @@
 {
   netplay = {
-    version = "3.4.6";
-    hash = "sha256-yjHoLOQpEXgjWMHxDiHYUmOP3e1zt3j13Gywx/PRe3w=";
+    version = "3.5.1";
+    hash = "sha256-/HngrIzlSTb4oTEr3z2vMHt0iXsIgELlk2BoSiW4Npk=";
   };
   netplay-beta = {
     version = "4.0.0-mainline-beta.11";
     hash = "sha256-r24UrhAmrLWlWmkHul2gkiX/p69R02Cuoxed20PcX1w=";
   };
   playback = {
-    version = "3.4.6";
-    hash = "sha256-bHbsdUs1pm7f/NSUn6f4s3LEL1Wbobq0h0TcQ0dCvbw=";
+    version = "3.5.1";
+    hash = "sha256-uXnvbPVNRIZn5wfPfb8jKGr/sBG87UFamFwSiOZZ3hg=";
   };
   launcher = {
     version = "2.11.10";
